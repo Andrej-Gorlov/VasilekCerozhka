4eacb95ebaa971f198134a48838dc0bc451b8918
Andrey_Gorlov <avgorlov899@gmail.com>
2023-01-29 01:22:55 +0300

    start

---
+++ VasilekCerozhka/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VasilekCerozhka.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
