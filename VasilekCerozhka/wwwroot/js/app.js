d651da77b79b0267eea2b5837089afc50617e88b
Andrej-Gorlov <avgorlov899@gmail.com>
2023-04-13 21:06:05 +0300

    add edit textarea

--- VasilekCerozhka/wwwroot/js/app.js
+++ VasilekCerozhka/wwwroot/js/app.js
@@ -10,15 +10,24 @@ function deleteCoupon() {
 }
 function TextareaChangesStyle() {
     document.getElementById("id_label_textarea").classList.add("label_textarea_focus");
+    document.getElementById("id_textarea").setAttribute('style', 'height:150px;background: #ebf3ff;');
+    document.getElementById("borderId").classList.add("border_textarea_custom");
+    document.getElementById("id_span_label").classList.remove("span_label_textarea");
 }
 function TextareaEmpty() {
     if (document.getElementById("id_textarea").value.length === 0) {
         document.getElementById("id_label_textarea").classList.remove("label_textarea_focus");
         document.getElementById("id_label_textarea").classList.add("label_textarea");
+        document.getElementById("id_textarea").setAttribute('style', 'height:35px;');
+        document.getElementById("borderId").classList.remove("border_textarea_custom");
+        document.getElementById("id_span_label").classList.add("span_label_textarea");
     }
 }
 function LoadTextarea() {
     if (document.getElementById("id_textarea").value.length > 0) {
+        document.getElementById("id_textarea").setAttribute('style', 'height:150px;background: #ebf3ff;');
+        document.getElementById("id_span_label").classList.remove("span_label_textarea");
+        document.getElementById("borderId").classList.add("border_textarea_custom");
         document.getElementById("id_label_textarea").classList.add("label_textarea_focus");
     }
     else {
